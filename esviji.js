28d539325dd92e7f16e161482eae25f5a1c994ff
Nicolas Hoizey <nicolas@hoizey.com>
2011-11-24 00:19:51 +0100

    appcache

--- esviji.js
+++ esviji.js
@@ -12,7 +12,7 @@ var esviji = {
   themes: {
     'metro': {
       'rock': 'metro',
-      'regularPieces': ['m1', 'm2', 'm3', 'm3bis', 'm4', 'm5', 'm6', 'm7', 'm7bis', 'm8', 'm9', 'm10', 'm11', 'm12', 'm13', 'm14']
+      'regularPieces': ['m1', 'm2', 'm3', 'm4', 'm5', 'm6', 'm7', 'm8', 'm9', 'm10', 'm11', 'm12', 'm13', 'm14']
     }
   },
   currentPieces: [],
