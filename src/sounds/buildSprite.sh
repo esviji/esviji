b0d4c8bf9b798c552546c8599f2fe80956dc32d8
Nicolas Hoizey <nicolas@hoizey.com>
2013-05-02 22:13:35 +0200

    Some explanations

--- src/sounds/buildSprite.sh
+++ src/sounds/buildSprite.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-# ffmpeg + ffmpeg2theora + audiosprite (https://github.com/tonistiigi/audiosprite) + underscore-cli
-# brew install libvorbis ?
-# http://v2v.cc/~j/ffmpeg2theora/
-# brew install ffmpeg
-# sudo npm install -g audiosprite
-# sudo npm install -g underscore-cli
+# This script generates an MP3 & OGG sprite from a folder of WAVs and the json needed by Howler.js
+# Uses ffmpeg + ffmpeg2theora + audiosprite (https://github.com/tonistiigi/audiosprite) + underscore-cli
+#
+# On a Mac:
+#   install http://v2v.cc/~j/ffmpeg2theora/
+#   run these 3 commands
+#     brew install ffmpeg
+#     sudo npm install -g audiosprite
+#     sudo npm install -g underscore-cli
+
 audiosprite --output ./sprite --export mp3 --channels 2 ./sources/*.wav
 ffmpeg2theora --output ./sprite.ogg ./sprite.mp3
 underscore -i ./sprite.json extract 'spritemap'  | underscore process "output = {}; output.urls = ['sounds/sprite.ogg', 'sounds/sprite.mp3']; output.sprite = {}; for (key in data) { output.sprite[key] = [data[key].start, data[key].end]; }; output" -o ./sprite-howler.json
