ab054ff023beafbf3d468b71a446b8f152443a07
Nicolas Hoizey <nicolas@hoizey.com>
2014-01-08 21:38:36 +0100

    cleaning the quotes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,35 +1,35 @@
 module.exports = function(grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-manifest');
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-growl');
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-less");
+  grunt.loadNpmTasks("grunt-manifest");
+  grunt.loadNpmTasks("grunt-autoprefixer");
+  grunt.loadNpmTasks("grunt-growl");
 
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
 
     watch: {
       less: {
         options: {
           debounceDelay: 250
         },
-        files: 'src/less/*.less',
-        tasks: ['growl:less', 'less', 'growl:autoprefixer', 'autoprefixer']
+        files: "src/less/*.less",
+        tasks: ["growl:less", "less", "growl:autoprefixer", "autoprefixer"]
       },
       js: {
         options: {
           debounceDelay: 250
         },
-        files: 'src/js/*.js',
-        tasks: ['growl:js', 'jshint']
+        files: "src/js/*.js",
+        tasks: ["growl:js", "jshint"]
       },
       manifest: {
         options: {
           debounceDelay: 250
         },
-        files: ['src/index.html', 'src/css/**', 'src/js/**', 'src/img/**', 'src/sounds/**'],
-        tasks: ['growl:manifest', 'manifest:src']
+        files: ["src/index.html", "src/css/**", "src/js/**", "src/img/**", "src/sounds/**"],
+        tasks: ["growl:manifest", "manifest:src"]
       }
     },
 
@@ -62,82 +62,110 @@ module.exports = function(grunt) {
 
     autoprefixer: {
       options: {
-        browsers: ['> 1%', 'last 2 versions', 'android >= 2']
+        browsers: ["> 1%", "last 2 versions", "android >= 2"]
       },
       no_dest: {
-        src: 'src/css/styles.css'
+        src: "src/css/styles.css"
       }
     },
 
     clean: {
       dist: {
-        src: 'dist/'
+        src: "dist/"
       }
     },
 
     copy: {
       dist: {
         files: [
-          { expand: true, cwd: 'src/', src: ['.htaccess', 'esviji-icon.png', 'favicon.ico', 'index.html', 'manifest.webapp'], dest: 'dist/' },
-          { expand: true, cwd: 'src/', src: ['css/font/*'], dest: 'dist/' },
-          { expand: true, cwd: 'src/', src: ['img/favicon.png', 'img/firefox-os/*', 'img/ios/*', 'img/windows-8/*'], dest: 'dist/' },
-          { expand: true, cwd: 'src/', src: ['sounds/sprite.{mp3,ogg}'], dest: 'dist/' }
+          {
+            expand: true,
+            cwd: "src/",
+            src: [".htaccess", "esviji-icon.png", "favicon.ico", "index.html", "manifest.webapp"],
+            dest: "dist/"
+          },
+          {
+            expand: true, 
+            cwd: "src/",
+            src: ["css/font/*"],
+            dest: "dist/"
+          },
+          {
+            expand: true,
+            cwd: "src/",
+            src: ["img/favicon.png", "img/firefox-os/*", "img/ios/*", "img/windows-8/*"],
+            dest: "dist/"
+          },
+          {
+            expand: true,
+            cwd: "src/",
+            src: ["sounds/sprite.{mp3,ogg}"],
+            dest: "dist/"
+          }
         ]
       }
     },
 
+    modernizr: {
+      "devFile" : "src/js/vendor/modernizr-*.js",
+      "outputFile" : "build/modernizr-custom.js",
+      "extra" : {
+        "shiv" : true,
+        "printshiv" : false,
+        "load" : true,
+        "mq" : false,
+        "cssclasses" : true
+      },
+      "uglify" : false
+    },
+
     useminPrepare: {
       options: {
-        dest: 'dist/'
+        dest: "dist/"
       },
-      html: 'src/index.html'
+      html: "src/index.html"
     },
     usemin: {
-      html: 'dist/index.html'
+      html: "dist/index.html"
     },
 
     concat: {
       options: {
-        separator: ';'
+        separator: ";"
       }
     },
 
     removelogging: {
       dist: {
-        src: 'dist/js/app.js',
+        src: "dist/js/app.js",
         options: {}
       }
     },
 
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> - Copyright (c) 1992-<%= grunt.template.today("yyyy") %> Nicolas Hoizey <nicolas@hoizey.com> */',
-        report:'min',
-        compress: {
-          global_defs: {
-            'DEBUG': false
-          }
-        }
+        banner: "/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today('yyyy-mm-dd') %> - Copyright (c) 1992-<%= grunt.template.today('yyyy') %> Nicolas Hoizey <nicolas@hoizey.com> */",
+        report:"min"
       }
     },
 
     cssmin: {
       options: {
-        banner: '/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> - Copyright (c) 1992-<%= grunt.template.today("yyyy") %> Nicolas Hoizey <nicolas@hoizey.com> */'
+        banner: "/*! <%= pkg.name %> v<%= pkg.version %> - <%= grunt.template.today('yyyy-mm-dd') %> - Copyright (c) 1992-<%= grunt.template.today('yyyy') %> Nicolas Hoizey <nicolas@hoizey.com> */"
       }
     },
 
     rev: {
       options: {
-        encoding: 'utf8',
-        algorithm: 'md5',
+        encoding: "utf8",
+        algorithm: "md5",
         length: 8
       },
       assets: {
         files: [{
           src: [
-            'dist/css/styles.css',
-            'dist/js/app.js'
+            "dist/css/styles.css",
+            "dist/js/app.js"
           ]
         }]
       }
@@ -146,73 +174,74 @@ module.exports = function(grunt) {
     manifest: {
       src: {
         options: {
-          basePath: 'src/',
-          network: ['*'],
+          basePath: "src/",
+          network: ["*"],
           verbose: true,
           timestamp: true
         },
         src: [
-          'favicon.ico',
-          'img/favicon.png',
-          'js/**/*.js',
-          'css/styles.css',
-          'css/font/*',
-          'sounds/*.{ogg,mp3}'
+          "favicon.ico",
+          "img/favicon.png",
+          "js/**/*.js",
+          "css/styles.css",
+          "css/font/*",
+          "sounds/*.{ogg,mp3}"
         ],
-        dest: 'src/manifest.appcache'
+        dest: "src/manifest.appcache"
       },
       dist: {
         options: {
-          basePath: 'dist/',
-          network: ['*'],
+          basePath: "dist/",
+          network: ["*"],
           verbose: true,
           timestamp: true
         },
         src: [
-          'favicon.ico',
-          'img/favicon.png',
-          'js/*.js',
-          'css/*.css',
-          'css/font/*',
-          'sounds/*.{ogg,mp3}'
+          "favicon.ico",
+          "img/favicon.png",
+          "js/*.js",
+          "css/*.css",
+          "css/font/*",
+          "sounds/*.{ogg,mp3}"
         ],
-        dest: 'dist/manifest.appcache'
+        dest: "dist/manifest.appcache"
       }
     },
 
     sed: {
       version: {
-        path: 'dist/',
+        path: "dist/",
         recursive: true,
-        pattern: '%VERSION%',
-        replacement: '<%= pkg.version %>'
+        pattern: "%VERSION%",
+        replacement: "<%= pkg.version %>"
       }
     },
 
     docco: {
       debug: {
-        src: ['src/js/esviji.js'],
+        src: ["src/js/esviji.js"],
         options: {
-          output: 'docs/'
+          output: "docs/"
         }
       }
     }
   });
 
-  grunt.registerTask('default', ['compile', 'watch']);
-  grunt.registerTask('compile', ['growl:less', 'less', 'growl:autoprefixer', 'autoprefixer', 'growl:manifest', 'manifest:src']);
-  grunt.registerTask('package', [], function() {
+  grunt.registerTask("default", ["compile", "watch"]);
+  grunt.registerTask("compile", ["growl:less", "less", "growl:autoprefixer", "autoprefixer", "growl:manifest", "manifest:src"]);
+  grunt.registerTask("package", [], function() {
     // cf https://github.com/gruntjs/grunt/issues/975#issuecomment-29058707
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-remove-logging');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-usemin');
-    grunt.loadNpmTasks('grunt-rev');
-    grunt.loadNpmTasks('grunt-sed');
-    grunt.loadNpmTasks('grunt-docco');
-    grunt.task.run('less', 'autoprefixer', 'clean', 'copy', 'useminPrepare', 'concat', 'removelogging', 'uglify', 'cssmin', 'rev', 'usemin', 'manifest:dist', 'sed', 'docco');
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+//    grunt.loadNpmTasks("grunt-modernizr");
+    grunt.loadNpmTasks("grunt-contrib-concat");
+    grunt.loadNpmTasks("grunt-remove-logging");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-cssmin");
+    grunt.loadNpmTasks("grunt-usemin");
+    grunt.loadNpmTasks("grunt-rev");
+    grunt.loadNpmTasks("grunt-sed");
+    grunt.loadNpmTasks("grunt-docco");
+    grunt.task.run("less", "autoprefixer", "clean", "copy", /*"modernizr"*/, "useminPrepare", "concat", "removelogging", "uglify", "cssmin", "rev", "usemin", "manifest:dist", "sed", "docco");
   });
 };
