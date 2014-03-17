335fc86eaae73c98383f8135bea2747c30372e66
Nicolas Hoizey <nicolas@hoizey.com>
2014-03-17 16:15:22 +0100

    Update vendors sources

--- src/js/vendor/offline.js
+++ src/js/vendor/offline.js
@@ -29,12 +29,12 @@
     checks: {
       xhr: {
         url: function() {
-          return "/offline-test-request/" + (Math.floor(Math.random() * 1000000000));
+          return "/favicon.ico?_=" + (Math.floor(Math.random() * 1000000000));
         }
       },
       image: {
         url: function() {
-          return "img/tiny-image.gif?_=" + (Math.floor(Math.random() * 1000000000));
+          return "/favicon.ico?_=" + (Math.floor(Math.random() * 1000000000));
         }
       },
       active: 'xhr'
