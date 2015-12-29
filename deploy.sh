e43e4a6a078b408a4371e260b9a04305906b2a2b
Nicolas Hoizey <nicolas@hoizey.com>
2015-12-29 23:44:04 +0100

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "Building esviji 2…" && grunt build && echo "Deploying esviji 2…" && scp -r build/* build/.htaccess nhoizey@ssh-nhoizey.alwaysdata.net:/home/nhoizey/www/esviji.com/v2/ && echo "Done"
