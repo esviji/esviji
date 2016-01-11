f0c0c26d4ccf53ad5b54a70bcbeb2705267ed6b9
Nicolas Hoizey <nicolas@hoizey.com>
2016-01-11 11:29:57 +0100

    Handle stable (master) and dev branches for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+START_TIME=$SECONDS
 
-echo "Building esviji 2…" && grunt build && echo "Deploying esviji 2…" && scp -r build/* build/.htaccess build/.well-known nhoizey@ssh-nhoizey.alwaysdata.net:/home/nhoizey/www/esviji.com/v2/ && echo "Done"
+currentBranch=$(git symbolic-ref --short HEAD)
+
+if [ "$currentBranch" == "master" ]
+then
+  echo "Compiling and deploying $currentBranch to play.esviji.com…"
+  grunt build && scp -r build/* build/.htaccess build/.well-known nhoizey@ssh-nhoizey.alwaysdata.net:/home/nhoizey/www/esviji.com/play/
+else
+  echo "Compiling and deploying $currentBranch to dev.esviji.com…"
+  grunt build && scp -r build/* build/.htaccess build/.well-known nhoizey@ssh-nhoizey.alwaysdata.net:/home/nhoizey/www/esviji.com/dev/
+fi
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "\nFinished in $ELAPSED_TIME seconds.\n\n"
