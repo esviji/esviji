47167ca5530144b57cb60f7be765d7a014920c9a
Nicolas Hoizey <nicolas@hoizey.com>
2020-04-21 23:31:38 +0200

    Add Service Worker with Workbox and Rollup

---
+++ workbox.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+const BUILD_DIR = 'build';
+
+module.exports = {
+  globDirectory: BUILD_DIR,
+  globPatterns: ['/', '**/*.{html,js,css,woff2,webmanifest}'],
+  swSrc: path.join(BUILD_DIR, 'service-worker.js'),
+  swDest: path.join(BUILD_DIR, 'service-worker.js'),
+  mode: 'production',
+};
